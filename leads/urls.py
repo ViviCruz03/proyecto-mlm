60bb447fb77bda43d2963db72d981a0343859772
ViviCruz03 <itzelcj.ti21@utsjr.edu.mx>
2025-03-18 17:57:49 -0600

    Consultas a partir de los estados, municipios y localidades de las unidades economicas

--- leads/urls.py
+++ leads/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, reverse
 from .views import *
 
+
 urlpatterns = [
     path('', home, name='index'),
+    path('consulta2/', UniEconomicaListView.as_view(), name='uniEconomicas-list'),
+    path('obtener-unidades/', obtener_unidades, name='obtener-unidades'),
+    path('obtener-condiciones/',obtener_condiciones, name='obtener-condiciones'),
+    path('obtener-filtros/', obtener_filtros, name='obtener-filtros'),
+    path('obtener-municipios/', obtener_municipios, name='obtener_municipios'),
+    path('obtener-localidades/', obtener_localidades, name='obtener_localidades'),
+    path('consultar-datos/', consultar_datos, name='consultar_datos')
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render, redirect
+from django.views.generic import CreateView, DetailView, ListView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 #Biblioteca de django que permite crear una coockie para la sesión del usuario
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
-from .models import *  # Importar el modelo para los estados
+from .models import *  
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+from django.http import JsonResponse
 
 
 
-# Create your views here.
+
+#Pantalla de home
 def home (request):
     return render(request, 'index.html')
 
+#Funcion de inicio de sesión
 def signin (request):
     if request.method == 'GET':
         return render(request, 'login.html',{
@@ -31,23 +36,105 @@ def signin (request):
             login(request, user)
             return redirect('accion')
 
+#Funcion de salir de sesión
 @login_required        
 def salir(request):
     logout(request)
     return redirect('index')
-        
+
+#Redirigir a la pantalla acción       
 @login_required
 def accion(request):
     return render(request, 'accion.html')
 
+#Crear consultas a partir de estado, municipio y localidad
 @login_required
 def crearConsulta(request):
     return render(request, 'consulta1.html',{'estados': Estado.ESTADOS})
+
+
+#Vista para enlistar las unidades economicas
+class UniEconomicaListView(ListView):
+    model = UniEconomicas
+    template_name = 'consulta2.html'
+    context_object_name = 'unis'
+
+#Obtener las unidades desde la base de datos
+def obtener_unidades(request):
+    unidades = UniEconomicas.objects.all().values('Nombre_de_la_Unidad_Economica', 'Descripcion_estrato_personal_ocupado', 'Numero_de_telefono')
+    return JsonResponse({'unidades': list(unidades)})
+
+#Filtrar las condiciones de las unidades
+def obtener_condiciones(request):
+    condiciones = UniEconomicas.objects.values_list('Nombre_de_clase_de_la_actividad', flat=True).distinct()
+    # print(f"Condiciones: {condiciones}")
+    if condiciones.exists():
+        return JsonResponse({'condiciones': list(condiciones)})
+    else:
+        return JsonResponse({'condiciones':[]}, status=404)
+
+#Filtrar los estados, municipios y localidades
+def obtener_filtros(request):
+    estados = UniEconomicas.objects.values_list('Entidad_federetiva', flat=True).distinct()
+    if estados.exists():
+        return JsonResponse({'estados': list(estados)})
+    else:
+        return JsonResponse({'estados': []}, status=404)
+
+#Devuelve municipios según el estado seleccionado
+def obtener_municipios(request):
+    estado = request.GET.get('estado', None)
+    # print(f"Estado recibido: {estado}")
+    if estado:
+        # Filtrar los municipios que pertenecen al estado solicitado
+        municipios = UniEconomicas.objects.filter(Entidad_federetiva__icontains=estado).values_list('Municipio', flat=True).distinct()
+        # print(f"Municipios encontrados: {municipios}")
+        return JsonResponse({'municipios': list(municipios)})
+    return JsonResponse({'municipios': []})
+
+
+#Devuelve localidades según el municipio seleccionado
+def obtener_localidades(request):
+    municipio = request.GET.get('municipio', None)
+    # print(f"municipio recibido: {municipio}")
+    if municipio:
+        # Filtrar las localidades que pertenecen al municipio solicitado
+        localidades = UniEconomicas.objects.filter(Municipio__icontains=municipio).values_list('Localidad', flat=True).distinct()
+        # print(f"localidades recibidas: {localidades}")
+        return JsonResponse({'localidades': list(localidades)})
+    return JsonResponse({'localidades': []})
+
+#Realizar la consulta a partir de las condiciones con est, mun, local
+def consultar_datos(request):
+    # Obtener los parámetros de la solicitud
+    condicion = request.GET.get('condicion', '').strip()
+    estado = request.GET.get('estado', '').strip()
+    municipio = request.GET.get('municipio', '').strip()
+    localidad = request.GET.get('localidad', '').strip()
+
+    # Construcción dinámica del filtro
+    filtros = {}
+    if condicion:
+        filtros['Nombre_de_clase_de_la_actividad__icontains'] = condicion
+    if estado:
+        filtros['Entidad_federetiva__icontains'] = estado
+    if municipio:
+        filtros['Municipio__icontains'] = municipio
+    if localidad:
+        filtros['Localidad__icontains'] = localidad
+
+    # Realizar la consulta con los filtros aplicados
+    resultados = UniEconomicas.objects.filter(**filtros).values('Nombre_de_la_Unidad_Economica', 'Descripcion_estrato_personal_ocupado', 'Numero_de_telefono')
+
+    # Convertir resultados a lista y devolver JSON
+    return JsonResponse({'resultados': list(resultados)})
 
 @login_required
 def consulta2(request):
     return render(request, 'consulta2.html',{"UniEconomicas": UniEconomicas})
 
+
+#Continuar el seguimiento de una consulta
 @login_required
 def segConsulta(request):
     return render(request, 'seguimiento1.html')
@@ -56,4 +143,5 @@ def segConsulta(request):
 @login_required
 def dash(request):
     return render(request, 'dash.html')
+
 
